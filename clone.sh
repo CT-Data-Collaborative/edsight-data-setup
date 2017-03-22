ca69547f9953d8a146bccc467132aa6bbf54b448
jennadaly <jenna.m.daly@gmail.com>
2017-03-22 17:48:48 -0400

    More cmdline scripts for processing.

---
+++ clone.sh
@@ -0,0 +1,20 @@
+cd ~/Desktop/Data/EdSight/District/
+
+
+cd CMT-CAPT-Science
+git clone git@github.com:CT-Data-Collaborative/ctdata-dataset-cookiecutter.git
+
+cd ../College-Entrance-and-Persistence
+git clone git@github.com:CT-Data-Collaborative/ctdata-dataset-cookiecutter.git
+
+cd ../Student-Enrollment
+git clone git@github.com:CT-Data-Collaborative/ctdata-dataset-cookiecutter.git
+
+cd ../Five-Year-Grad-Rates
+git clone git@github.com:CT-Data-Collaborative/ctdata-dataset-cookiecutter.git
+
+cd ../Four-Year-Grad-Rates
+git clone git@github.com:CT-Data-Collaborative/ctdata-dataset-cookiecutter.git
+
+cd ../Suspension-Rates
+git clone git@github.com:CT-Data-Collaborative/ctdata-dataset-cookiecutter.git
---
+++ git_add.sh
@@ -0,0 +1,299 @@
+cd ~/Desktop/Data/EdSight/District/CMT-CAPT-Science
+
+cd CMT-CAPT-Science-by-Gender
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/cmt-capt-science-by-gender.git
+git push origin master
+
+cd CMT-CAPT-Science-by-Race-Ethnicity
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/cmt-capt-science-by-race-ethnicity.git
+git push origin master
+
+cd ../CMT-CAPT-Science-by-Special-Education-Status
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/cmt-capt-science-by-special-education-status.git
+git push origin master
+
+cd ../CMT-CAPT-Science-by-Meal-Eligibility-Lvl-2
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/cmt-capt-science-by-meal-eligibility-lvl-2.git
+git push origin master
+
+cd ../CMT-CAPT-Science-by-Meal-Eligibility-Lvl-3
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/cmt-capt-science-by-meal-eligibility-lvl-3.git
+git push origin master
+
+cd ../CMT-CAPT-Science-by-ELL
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/cmt-capt-science-by-ELL.git
+git push origin master
+
+cd ../CMT-CAPT-Science-by-All-Students
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/cmt-capt-science-by-all-students.git
+git push origin master
+
+cd ../CMT-CAPT-Science-by-High-Needs
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/cmt-capt-science-by-high-needs.git  
+
+cd ../../College-Entrance-and-Persistence/College-Entrance-and-Persistence-by-Gender
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/college-entrance-and-persistence-by-gender.git
+git push origin master
+
+cd ../College-Entrance-and-Persistence-by-Race-Ethnicity
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/college-entrance-and-persistence-by-race-ethnicity.git
+git push origin master
+
+cd ../College-Entrance-and-Persistence-by-Special-Education-Status
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/college-entrance-and-persistence-by-special-education-status.git
+git push origin master
+
+cd ../College-Entrance-and-Persistence-by-Meal-Eligibility
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/college-entrance-and-persistence-by-meal-eligibility.git
+git push origin master
+
+cd ../College-Entrance-and-Persistence-by-ELL
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/college-entrance-and-persistence-by-ELL.git
+git push origin master
+
+cd ../College-Entrance-and-Persistence-by-All-Students
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/college-entrance-and-persistence-by-all-students.git 
+git push origin master
+
+
+cd ../../Student-Enrollment/Student-Enrollment-by-Grade
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/student-enrollment-by-grade.git
+git push origin master
+
+cd ../Student-Enrollment-by-Gender
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/student-enrollment-by-gender.git
+git push origin master
+
+cd ../Student-Enrollment-by-Race-Ethnicity
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/student-enrollment-by-race-ethnicity.git
+git push origin master
+
+cd ../Student-Enrollment-by-Special-Education-Status
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/student-enrollment-by-special-education-status.git
+git push origin master
+
+cd ../Student-Enrollment-by-Meal-Eligibility
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/student-enrollment-by-meal-eligibility.git
+git push origin master
+
+cd ../Student-Enrollment-by-ELL
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/student-enrollment-by-ELL.git
+git push origin master
+
+cd ../Student-Enrollment-by-All-Students
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/student-enrollment-by-all-students.git 
+git push origin master
+
+cd ../../Five-Year-Grad-Rates/Five-Year-Grad-Rates-by-Gender
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/five-year-grad-rates-by-gender.git
+git push origin master
+
+cd ../Five-Year-Grad-Rates-by-Race-Ethnicity
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/five-year-grad-rates-by-race-ethnicity.git
+git push origin master
+
+cd ../Five-Year-Grad-Rates-by-Special-Education-Status
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/five-year-grad-rates-by-special-education-status.git.git
+git push origin master
+
+cd ../Five-Year-Grad-Rates-by-Meal-Eligibility
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/five-year-grad-rates-by-meal-eligibility.git
+git push origin master
+
+cd ../Five-Year-Grad-Rates-by-ELL
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/five-year-grad-rates-by-ELL.git
+git push origin master
+
+cd ../Five-Year-Grad-Rates-by-All-Students
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/five-year-grad-rates-by-all-students.git
+git push origin master
+
+cd ../Five-Year-Grad-Rates-by-High-Needs
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/five-year-grad-rates-by-high-needs.git 
+git push origin master
+
+
+cd ../../Four-Year-Grad-Rates/Four-Year-Grad-Rates-by-Gender
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/four-year-grad-rates-by-gender.git
+git push origin master
+
+cd ../Four-Year-Grad-Rates-by-Race-Ethnicity
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/four-year-grad-rates-by-race-ethnicity.git
+git push origin master
+
+cd ../Four-Year-Grad-Rates-by-Special-Education-Status
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/four-year-grad-rates-by-special-education-status.git
+git push origin master
+
+cd ../Four-Year-Grad-Rates-by-Meal-Eligibility
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/four-year-grad-rates-by-meal-eligibility.git
+git push origin master
+
+cd ../Four-Year-Grad-Rates-by-ELL
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/four-year-grad-rates-by-ELL.git
+git push origin master
+
+cd ../Four-Year-Grad-Rates-by-All-Students
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/four-year-grad-rates-by-all-students.git
+git push origin master
+
+cd ../Four-Year-Grad-Rates-by-High-Needs
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/four-year-grad-rates-by-high-needs.git 
+git push origin master
+
+
+cd ../../Suspension-Rates/Suspension-Rates-by-Gender
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/suspension-rates-by-gender.git
+git push origin master
+
+cd ../Suspension-Rates-by-Race-Ethnicity
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/suspension-rates-by-race-ethnicity.git
+git push origin master
+
+cd ../Suspension-Rates-by-Special-Education-Status
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/suspension-rates-by-special-education-status.git
+git push origin master
+
+cd ../Suspension-Rates-by-Meal-Eligibility
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/suspension-rates-by-meal-eligibility.git
+git push origin master
+
+cd ../Suspension-Rates-by-ELL
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/suspension-rates-by-ELL.git
+git push origin master
+
+cd ../Suspension-Rates-by-All-Students
+git init
+git add *
+git commit -m "Initial commit."
+git remote add origin git@github.com:CT-Data-Collaborative/suspension-rates-by-all-students.git 
+git push origin master
+
+
+
+
+
+
+
+
+
